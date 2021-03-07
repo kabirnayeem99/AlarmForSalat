130466e2836ffa72b144de324953346e0fac3163
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-07 11:52:09 +0600

    📦 new: initial commit

---
+++ app/src/main/java/io/github/kabirnayeem99/alarmforsalat/AlarmForSalatActivity.kt
@@ -0,0 +1,11 @@
+package io.github.kabirnayeem99.alarmforsalat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmForSalatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_for_salat)
+    }
+}
