e359617b0bacb09752c4e7f1b6af0f27a75c79d6
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-08 21:48:28 +0600

    👌 improve: implements location request permission

--- app/src/main/java/io/github/kabirnayeem99/alarmforsalat/ui/activities/AlarmForSalatActivity.kt
+++ app/src/main/java/io/github/kabirnayeem99/alarmforsalat/ui/activities/AlarmForSalatActivity.kt
@@ -1,13 +1,17 @@
 package io.github.kabirnayeem99.alarmforsalat.ui.activities
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import io.github.kabirnayeem99.alarmforsalat.R
 import io.github.kabirnayeem99.alarmforsalat.adapters.PagerAdapter
 import io.github.kabirnayeem99.alarmforsalat.databinding.ActivityAlarmForSalatBinding
 import io.github.kabirnayeem99.alarmforsalat.ui.fragments.AlarmFragment
 import io.github.kabirnayeem99.alarmforsalat.ui.fragments.LocationFragment
 
+const val ACCESS_CODE_LOCATION = 1
 
 class AlarmForSalatActivity : AppCompatActivity() {
     private lateinit var fragmentAlarm: AlarmFragment
@@ -20,6 +24,40 @@ class AlarmForSalatActivity : AppCompatActivity() {
         setContentView(binding.root)
         initFragments()
         initTabLayout()
+        requestLocationPermission()
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                ACCESS_CODE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            ACCESS_CODE_LOCATION -> when {
+                grantResults[0] != PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(this, "You didn't grant this permission", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                else -> {
+                    Toast.makeText(this, "You granted this permission", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun initTabLayout() {
@@ -39,10 +77,4 @@ class AlarmForSalatActivity : AppCompatActivity() {
         fragmentLocation = LocationFragment()
     }
 
-//    private fun makeCurrentFragment(fragment: Fragment) {
-//        supportFragmentManager.beginTransaction().apply {
-//            replace(binding.flFragmentHolder.id, fragment)
-//            commit()
-//        }
-//    }
 }
