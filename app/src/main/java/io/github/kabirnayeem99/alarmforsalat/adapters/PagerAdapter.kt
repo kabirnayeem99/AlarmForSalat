09a4cb8cbb7a92a6098d5aa9ebc092a9ba708b04
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-09 12:32:35 +0600

    👌 improve: adds date fragment in pager adapter

--- app/src/main/java/io/github/kabirnayeem99/alarmforsalat/adapters/PagerAdapter.kt
+++ app/src/main/java/io/github/kabirnayeem99/alarmforsalat/adapters/PagerAdapter.kt
@@ -5,20 +5,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import io.github.kabirnayeem99.alarmforsalat.ui.fragments.AlarmFragment
+import io.github.kabirnayeem99.alarmforsalat.ui.fragments.DateFragment
 import io.github.kabirnayeem99.alarmforsalat.ui.fragments.MapsFragment
 
 
 class PagerAdapter(fm: FragmentManager, var context: Context) :
     FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getCount(): Int {
-        return 2
+        return 3
     }
 
     override fun getItem(position: Int): Fragment {
         var fragment: Fragment = AlarmFragment()
         when (position) {
             0 -> fragment = AlarmFragment.instance
-            1 -> fragment = MapsFragment.instance
+            1 -> fragment = DateFragment.instance
+            2 -> fragment = MapsFragment.instance
         }
         return fragment
     }
