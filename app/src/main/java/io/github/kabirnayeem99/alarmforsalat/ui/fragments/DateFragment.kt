54cbf676f0ed31e0d749edae8884cbce2dbeae4a
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-09 12:23:09 +0600

    👌 improve: adds new fragment
    adds date fragment

---
+++ app/src/main/java/io/github/kabirnayeem99/alarmforsalat/ui/fragments/DateFragment.kt
@@ -0,0 +1,38 @@
+package io.github.kabirnayeem99.alarmforsalat.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.github.kabirnayeem99.alarmforsalat.R
+import io.github.kabirnayeem99.alarmforsalat.databinding.FragmentAlarmBinding
+import io.github.kabirnayeem99.alarmforsalat.databinding.FragmentDateBinding
+
+class DateFragment : Fragment() {
+    /*
+    follows https://developer.android.com/topic/libraries/view-binding
+    for view binding
+  */
+    private var _binding: FragmentDateBinding? = null
+
+    private val binding get() = _binding!!
+
+    companion object {
+        val instance: DateFragment by lazy(LazyThreadSafetyMode.PUBLICATION) { DateFragment() }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
