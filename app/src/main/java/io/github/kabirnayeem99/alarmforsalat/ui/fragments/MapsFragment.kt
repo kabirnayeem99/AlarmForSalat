d90dfa1d5f96879a8408c1cb9a1764edd5591ee3
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-11 14:11:27 +0600

    👌 improve: adds fun to load cities from json

--- app/src/main/java/io/github/kabirnayeem99/alarmforsalat/ui/fragments/MapsFragment.kt
+++ app/src/main/java/io/github/kabirnayeem99/alarmforsalat/ui/fragments/MapsFragment.kt
@@ -1,21 +1,26 @@
 package io.github.kabirnayeem99.alarmforsalat.ui.fragments
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.gson.Gson
 import io.github.kabirnayeem99.alarmforsalat.R
-import io.github.kabirnayeem99.alarmforsalat.databinding.FragmentAlarmBinding
+import io.github.kabirnayeem99.alarmforsalat.Utils.Constants
+import io.github.kabirnayeem99.alarmforsalat.Utils.Utilities
+import io.github.kabirnayeem99.alarmforsalat.data.view_objects.City
+import io.github.kabirnayeem99.alarmforsalat.data.view_objects.PlacesResponse
 import io.github.kabirnayeem99.alarmforsalat.databinding.FragmentMapsBinding
+import java.io.InputStream
+import java.nio.charset.Charset
 
 class MapsFragment : Fragment() {
 
@@ -26,15 +31,36 @@ class MapsFragment : Fragment() {
     private var _binding: FragmentMapsBinding? = null
 
     private val binding get() = _binding!!
+    lateinit var placesResponse: PlacesResponse
 
     private val callback = OnMapReadyCallback { googleMap ->
-        val ctg = LatLng(22.341685874789473, 91.7815618476437)
-        googleMap.addMarker(MarkerOptions().position(ctg).title("Marker in ctg"))
+        val cities = retrieveCities()
+        val city = cities[0]
+        val ctg = LatLng(city.lat, city.lng)
+        googleMap.addMarker(
+            MarkerOptions().position(ctg).title("Marker in ${placesResponse[0].city}")
+        )
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ctg, 14f))
+    }
+
+    private fun retrieveCities(): List<City> {
+
+        val inputStream: InputStream = requireActivity().assets.open(Constants.placesFileName)
+
+        val json: String? = Utilities.fileToJson(inputStream)
+
+        if (json != null) {
+            placesResponse = Gson().fromJson(json, PlacesResponse::class.java)
+            Toast.makeText(requireContext(), placesResponse[0].city, Toast.LENGTH_SHORT).show()
+        } else {
+            return listOf(City("", "", 0.0, 0.0, 20))
+        }
+        return placesResponse
     }
 
     companion object {
         val instance: MapsFragment by lazy(LazyThreadSafetyMode.PUBLICATION) { MapsFragment() }
+        private const val TAG = "MapsFragment"
     }
 
     override fun onCreateView(
@@ -42,7 +68,8 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
