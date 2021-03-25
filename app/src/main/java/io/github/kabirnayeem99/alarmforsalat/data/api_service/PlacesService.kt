f768fd29522f1b1d486dedf38f817316f1ef3249
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-25 12:55:01 +0600

    refactor (unused class): Delete unused class

--- app/src/main/java/io/github/kabirnayeem99/alarmforsalat/data/api_service/PlacesService.kt
+++
@@ -1,7 +0,0 @@
-package io.github.kabirnayeem99.alarmforsalat.data.api_service
-
-import retrofit2.http.GET
-
-interface PlacesService {
-//    @GET("")
-}
--- app/src/main/java/io/github/kabirnayeem99/alarmforsalat/enum/Tabs.kt
+++
@@ -1,7 +0,0 @@
-package io.github.kabirnayeem99.alarmforsalat.enum
-
-enum class Tabs {
-    DATE,
-    ALARM,
-    LOCATION,
-}
