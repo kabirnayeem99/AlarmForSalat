3a16aa0b6b79bef7ca01267fad62ba2ab691a594
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-25 12:53:01 +0600

    doc (enum): Document works of the functions

--- app/src/main/java/io/github/kabirnayeem99/alarmforsalat/enum/Meridiem.kt
+++ app/src/main/java/io/github/kabirnayeem99/alarmforsalat/enum/Meridiem.kt
@@ -1,5 +1,10 @@
 package io.github.kabirnayeem99.alarmforsalat.enum
 
+
+/**
+ * By convention, AM denotes ante
+ * meridiem and PM denotes post meridiem.
+ */
 enum class Meridiem {
     AM,
     PM,
